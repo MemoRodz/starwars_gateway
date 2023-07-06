71822f804ab67a35b6961e21ccb679fac4dc999f
MemoRodz <gmo.rodriguez@gmail.com>
2023-07-06 14:42:21 -0600

    Servicio Database Dockerizado

--- characters/src/data/index.js
+++ characters/src/data/index.js
@@ -3,10 +3,10 @@ const axios = require("axios");
 
 module.exports = {
     list: async () => {
-        return await axios.get("http://localhost:8004/Character");
+        return await axios.get("http://database:8004/Character");
     },
     getById: async (_id) => {
-        return await axios.get(`http://localhost:8004/Character/${_id}`);
+        return await axios.get(`http://database:8004/Character/${_id}`);
     },
     create: async () => {
         throw Error("Hay un error al momento de crear el personaje.");
--- films/src/data/index.js
+++ films/src/data/index.js
@@ -5,10 +5,10 @@ const axios = require("axios");
 module.exports = {
     list: async () => {
         //return films;
-        return await axios.get("http://localhost:8004/Film");
+        return await axios.get("http://database:8004/Film");
     },
     getById: async(_id) => {
-        return await axios.get(`http://localhost:8004/Film/${_id}`)
+        return await axios.get(`http://database:8004/Film/${_id}`)
     },
     create: async () => {
         throw Error("Hay un error al momento de crear el filme.");
